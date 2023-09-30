5f776237964fa4d26bf479310cd5a9840b0e5307
Eduardo Feo <eduardo@idsia.ch>
2023-09-30 22:03:47 +0300

    another answer for name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,6 +1,6 @@
 export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("name")) {
-    return ("My name is efeoflus");
+    return ("test3");
   }
 
   return "";
